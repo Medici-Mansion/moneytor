8bfe7e189fbb9d7c7a94b5a2a6667b5f2ef2dce9
Raymond <akdfhr2@gmail.com>
2024-08-10 09:03:48 +0900

    prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,10 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
   plugins: ["prettier-plugin-tailwindcss"],
+  singleQuote: true,
+  endOfLine: "auto",
+  jsxSingleQuote: true,
+  semi: false,
 };
 
 export default config;
